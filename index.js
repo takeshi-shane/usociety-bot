bec3cea35bf0e2b535d7adf16dc2ff6718a599aa
takeshi-shane <68736862+takeshi-shane@users.noreply.github.com>
2020-07-24 18:58:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -94,7 +94,7 @@ client.on('message', msg => {
         } else { msg.reply("nu ai acces la aceasta comanda!"); }
     }
     else if(command === "status") {
-    	GetServerPlayers(msg, "rpg.crowland.ro", 'Crowland');
+    	GetServerPlayers(msg, "193.203.39.228", 'Crowland');
     	GetServerPlayers(msg, "193.203.39.214", 'Eclipsed');
 	GetServerPlayers(msg, "193.203.39.36", 'Nephrite');
     }
